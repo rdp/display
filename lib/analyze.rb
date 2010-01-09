7989c8b1f5f9bf42b24b0ab2478cae8be1022f87
rdp <rogerpack2005@gmail.com>
2010-01-09 08:18:28 -0700

    first working version--switch to using redparse

--- lib/analyze.rb
+++ lib/analyze.rb
@@ -1,17 +1,33 @@
-require 'ruby_parser'
+require 'redparse' # takes forever on doze [ltodo]
+
 class Object
   def analyze *args
-   opts = args[-1].is_a? Hash ? args.pop : {}
-   _dbg
    a = caller
    last_caller = a[0] # "spec.analyze:9" # TODO work with full paths
-   file, line, *rest = a.split(":") # TODO cache shtuff
-   exact_line = File.readlines(file)[line.to_i].strip
-   out = "#{file}:#{line} was #{exact_line}"
-   if opts[:string]
-    return out
-   else
-    puts out
-   end
+   file, line, *rest = last_caller.split(":") # TODO cache shtuff
+   exact_line = File.readlines(file)[line.to_i - 1].strip
+   parser=RedParse.new(exact_line)
+   tree = parser.parse
+   out = nil
+   # the trick is to break out with the first method call...
+   right_call_node = give_me_first_call_node tree
+   # eureka
+   out = "#{file}:#{line} "
+   right_call_node.params.each{ |p|
+    out += "#{p.name}=#{args.shift}"
+   }
+   puts out
+   out
   end
+  
+  def give_me_first_call_node tree
+    tree.walk{|parent,i,subi,node|
+      if node.class == RedParse::CallNode
+        return tree
+      else
+        return give_me_first_call_node(node.right)
+      end
+    }
+  end
+  
 end
